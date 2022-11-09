43f2a327c088c94b5e5e3cad320cf1e0027bfc68
JoaoLeite47 <StalkerJJ@outlook.com>
2022-11-08 21:33:09 -0300

    feat<Register> Register page has created

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "Register.css";
+import "../../Global.css"
+
+export default function Register() {
+  return (
+    <div>
+      <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form"></form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import LoginCreateCount from "../../components/Login/LoginCreateCount/LoginCreat
 import LoginInputs from "../../components/Login/LoginInputs/LoginInputs";
 import LoginTitulo from "../../components/Login/LoginTitulo/LoginTitulo";
 import "./login.css";
+import "../../Global.css";
 
 export default function Login() {
   return (
